f594e3fe390c3a0fa9ccf751e2bc0517d1edf506
Sriram8788 <54744038+Sriram8788@users.noreply.github.com>
2024-12-21 09:59:27 +0530

    Update jenkinsdata.sh

--- terraform-jenkins/jenkinsdata.sh
+++ terraform-jenkins/jenkinsdata.sh
@@ -1,31 +1,42 @@
 #! /bin/bash
 # update os
-yum update -y
+sudo yum update -y
+
 # set server hostname as jenkins-server
-hostnamectl set-hostname jenkins-server
+sudo hostnamectl set-hostname jenkins-server
+
 # install git
-yum install git -y
-# install java 11
-dnf install java-11-amazon-corretto -y
+sudo yum install git -y
+
+# install java 17 (Amazon Corretto)
+sudo yum install java-17-amazon-corretto -y
+
+# verify Java 17 installation
+java -version
+
 # install jenkins
-wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-yum upgrade
-amazon-linux-extras install java-openjdk11 -y
-dnf install java-11-amazon-corretto -y
-yum install jenkins -y
-systemctl daemon-reload
-systemctl start jenkins
-systemctl enable jenkins
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+sudo yum install jenkins -y
+
+# reload and start Jenkins service
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
 # install docker
-amazon-linux-extras install docker -y
-systemctl start docker
-systemctl enable docker
-usermod -a -G docker ec2-user
-usermod -a -G docker jenkins
+sudo amazon-linux-extras install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# add the ec2-user and jenkins to the docker group
+sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker jenkins
+
 # configure docker as cloud agent for jenkins
-cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
-sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
-systemctl daemon-reload
-systemctl restart docker
-systemctl restart jenkins
+sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
+sudo sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo systemctl restart jenkins
